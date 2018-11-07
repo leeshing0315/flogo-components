233f27f9508fc006472df91b45a9016eb697c2d1
Max C Li <max.c.li@cargosmart.com>
2018-11-07 14:22:40 +0800

    add Activity solace-publisher

---
+++ activity/solace-publisher/activity.go
@@ -0,0 +1,130 @@
+package SolacePublisher
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"pack.ag/amqp"
+)
+
+var log = logger.GetLogger("activity-solacepublisher")
+
+var senderLock sync.Mutex
+
+var storedHostIP string
+
+var storedVpnName string
+
+var storedTopicName string
+
+var storedClient *amqp.Client
+
+var storedSession *amqp.Session
+
+var storedSender *amqp.Sender
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(actCtx activity.Context) (done bool, err error) {
+
+	// do eval
+	hostIP := actCtx.GetInput("hostIP").(string)
+	vpnName := actCtx.GetInput("vpnName").(string)
+	topicName := actCtx.GetInput("topicName").(string)
+	data := actCtx.GetInput("data").(string)
+
+	// 1. if many data send in same time, it will create many connections that may over solace connection number limit, you can run the activity_test and switch the TEST_ROUTINE_NUM to 600 or 700
+	// 2. so we use mutex lock temporarily now
+	// TODO 3. check which amqp object(client, session, sender) are concurrent safe, and use github.com/jolestar/go-commons-pool to pooling it's children
+	senderLock.Lock()
+	defer senderLock.Unlock()
+	sender, err := getSender(hostIP, vpnName, topicName)
+
+	if err != nil {
+		actCtx.SetOutput("publishSuccess", "false")
+		return false, err
+	}
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	// defer sender.Close(ctx)
+	defer cancel()
+	err = sender.Send(ctx, amqp.NewMessage([]byte(data)))
+
+	if err != nil {
+		sender.Close(ctx)
+		storedSender = nil
+		clearStore()
+
+		log.Errorf("AMQP Send error: [%s]", err)
+		actCtx.SetOutput("publishSuccess", "false")
+		return false, err
+	}
+
+	log.Debugf("Solace publisher send successfully by topicName [%s] and data [%s]", topicName, data)
+	actCtx.SetOutput("publishSuccess", "true")
+	return true, nil
+}
+
+func getSender(hostIP string, vpnName string, topicName string) (*amqp.Sender, error) {
+	if hostIP != storedHostIP || vpnName != storedVpnName || topicName != storedTopicName || storedClient == nil || storedSession == nil || storedSender == nil {
+		err := createSender(hostIP, vpnName, topicName)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return storedSender, nil
+}
+
+func createSender(hostIP string, vpnName string, topicName string) error {
+	client, err := amqp.Dial("amqp://" + hostIP + "/" + vpnName)
+	if err != nil {
+		log.Errorf("AMQP Dial fail by hostIP [%s] and vpnName [%s] and error is [%s]", hostIP, vpnName, err)
+		return err
+	}
+	session, err := client.NewSession()
+	if err != nil {
+		log.Error("AMQP NewSession fail and error is [%s]", err)
+		return err
+	}
+	sender, err := session.NewSender(amqp.LinkTargetAddress("topic://" + topicName))
+	if err != nil {
+		log.Errorf("AMQP NewSender fail by topicName [%s] and error is [%s]", topicName, err)
+		return err
+	}
+	storedHostIP = hostIP
+	storedVpnName = vpnName
+	storedTopicName = topicName
+	storedClient = client
+	storedSession = session
+	storedSender = sender
+	return nil
+}
+
+func clearStore() {
+	storedHostIP = ""
+	storedVpnName = ""
+	storedTopicName = ""
+	if storedSession == nil {
+		storedSession.Close(context.Background())
+	}
+	if storedClient == nil {
+		storedClient.Close()
+	}
+}
---
+++ activity/solace-publisher/activity_test.go
@@ -0,0 +1,127 @@
+package SolacePublisher
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/stretchr/testify/assert"
+)
+
+const GPS_EVENT_SAMPLE = "{ " +
+	"\"seqno\" : \"11129352\", " +
+	"\"cntrNum\" : \"CXRU1035624\", " +
+	"\"revTime\" : \"2018-07-19T00:00:48+08:00\", " +
+	"\"cltTime\" : \"2018-07-19T00:00:31+08:00\", " +
+	"\"locateTime\" : \"2018-07-19T00:00:31+08:00\", " +
+	"\"eleState\" : \"1\", " +
+	"\"batLevel\" : \"100\", " +
+	"\"opMode\" : \"Modulation\", " +
+	"\"setTem\" : \"0\", " +
+	"\"supTem\" : \"0.2\", " +
+	"\"retTem\" : \"2.3\", " +
+	"\"lng\" : \"139.759088\", " +
+	"\"lat\" : \"35.624676\", " +
+	"\"speed\" : \"0\", " +
+	"\"direction\" : \"340\", " +
+	"\"hpt\" : \"1090\", " +
+	"\"posFlag\" : \"1\", " +
+	"\"ism\" : \"0\", " +
+	"\"gpsNum\" : \"12\", " +
+	"\"bdNum\" : \"12\", " +
+	"\"lpt\" : \"30\", " +
+	"\"pt\" : \"409.6\", " +
+	"\"ct1\" : \"13.1\", " +
+	"\"ct2\" : \"9.2\", " +
+	"\"ambs\" : \"29.9\", " +
+	"\"eis\" : \"-0.7\", " +
+	"\"eos\" : \"-0.5\", " +
+	"\"dchs\" : \"78.1\", " +
+	"\"sgs\" : \"-19.5\", " +
+	"\"smv\" : \"13.1\", " +
+	"\"ev\" : \"81\", " +
+	"\"dss\" : \"0\", " +
+	"\"drs\" : \"2.1\", " +
+	"\"source\" : \"WEB_SERVICE\", " +
+	"\"carrier\" : \"COSU\", " +
+	"\"displayName\" : \"Shinagawa, Tokyo, Japan\", " +
+	"\"address\" : {" +
+	"\"distance\" : 1.5332328884126547, " +
+	"\"longitude\" : 139.748, " +
+	"\"latitude\" : 35.6044, " +
+	"\"city\" : \"Shinagawa\", " +
+	"\"region_code\" : \"13\", " +
+	"\"region\" : \"Tokyo\", " +
+	"\"country_code\" : \"JP\", " +
+	"\"country\" : \"Japan\"" +
+	"}, " +
+	"}"
+
+const (
+	TEST_HOSTIP      = "localhost:5672"
+	TEST_VPN         = "default"
+	TEST_TOPICNAME   = "TOPIC/TEST"
+	TEST_DATA        = GPS_EVENT_SAMPLE
+	TEST_ROUTINE_NUM = 10000
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("hostIP", TEST_HOSTIP)
+	tc.SetInput("vpnName", TEST_VPN)
+	tc.SetInput("topicName", TEST_TOPICNAME)
+	tc.SetInput("data", TEST_DATA)
+	ch := make(chan string)
+	for i := 0; i < TEST_ROUTINE_NUM; i++ {
+		go concurrent(ch, act, tc, t)
+	}
+	for i := 0; i < TEST_ROUTINE_NUM; i++ {
+		<-ch
+	}
+}
+
+func concurrent(ch chan string, act activity.Activity, tc *test.TestActivityContext, t *testing.T) {
+	act.Eval(tc)
+	result := tc.GetOutput("publishSuccess")
+	assert.Equal(t, "true", result)
+	ch <- result.(string)
+}
