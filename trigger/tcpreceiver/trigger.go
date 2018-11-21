202eef90615003da8b33a983f1776403d54304c9
Max C Li <max.c.li@cargosmart.com>
2018-11-21 17:13:27 +0800

    [tcpreceiver] fix command data type

--- trigger/tcpreceiver/trigger.go
+++ trigger/tcpreceiver/trigger.go
@@ -68,7 +68,7 @@ func (t *MyTrigger) Start() error {
 		log.Println(s.Conn.RemoteAddr().String(), packet)
 
 		triggerData := map[string]interface{}{}
-		triggerData["command"] = packet.Command
+		triggerData["command"] = int(packet.Command)
 		triggerData["packet"] = packet
 		triggerData["ip"] = s.Conn.RemoteAddr().String()
 		triggerData["eventTime"] = time.Now().Format("2006-01-02 15:04:05.000000")
