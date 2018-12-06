cbbeb9c4c20895259913817198f787f56646f5dd
Max C Li <max.c.li@cargosmart.com>
2018-12-06 11:07:22 +0800

    add GenGpsEventFromSinglePacket

--- common/entity/gpsevent.go
+++ common/entity/gpsevent.go
@@ -1,5 +1,7 @@
 package entity
 
+import "time"
+
 // GpsEvent entity gpsEvent
 type GpsEvent struct {
 	ID string `json:"id"`
@@ -52,6 +54,11 @@ type GpsEvent struct {
 	Drs       string `json:"drs"`
 }
 
+const (
+	GPSEVENT_SOURCE_TCPSERVER = "TCP_SERVER"
+	GPSEVENT_CARRIER_COSU     = "COSU"
+)
+
 // GpsEventAddress entity gpsEventAddress
 type GpsEventAddress struct {
 	distance  float64
@@ -63,4 +70,60 @@ type GpsEventAddress struct {
 	region      string
 	CountryCode string `json:"country_code"`
 	country     string
+}
+
+func GenGpsEventFromSinglePacket(singlePacket *SinglePacket, seqNo string, cntrNum string) *GpsEvent {
+	gpsEvent := &GpsEvent{}
+
+	gpsEvent.Seqno = seqNo
+	gpsEvent.CntrNum = cntrNum
+	gpsEvent.RevTime = time.Now().Format("2018-12-03T09:29:21+08:00")
+	gpsEvent.CltTime = singlePacket.Date
+	gpsEvent.LocateTime = singlePacket.Date
+	gpsEvent.EleState = ReturnValueByCondition(singlePacket.PowerSupplyStatus, "1", "0")
+	gpsEvent.BatLevel = singlePacket.BatLevel
+	gpsEvent.OpMode = ReturnValueByCondition(singlePacket.InfoItem.OpModeValid, singlePacket.InfoItem.OpMode, "")
+	gpsEvent.SetTem = ReturnValueByCondition(singlePacket.InfoItem.SetTemValid, singlePacket.InfoItem.SetTem, "")
+	gpsEvent.SupTem = ReturnValueByCondition(singlePacket.InfoItem.SupTemValid, singlePacket.InfoItem.SupTem, "")
+	gpsEvent.RetTem = ReturnValueByCondition(singlePacket.InfoItem.RetTemValid, singlePacket.InfoItem.RetTem, "")
+	gpsEvent.Hum = ReturnValueByCondition(singlePacket.InfoItem.HumValid, singlePacket.InfoItem.Hum, "")
+	gpsEvent.Lat = singlePacket.Lat
+	gpsEvent.Lng = singlePacket.Lng
+	gpsEvent.Speed = singlePacket.Speed
+	gpsEvent.Direction = singlePacket.Direction
+	gpsEvent.Hpt = ReturnValueByCondition(singlePacket.InfoItem.HptValid, singlePacket.InfoItem.Hpt, "")
+	gpsEvent.Usda1 = ReturnValueByCondition(singlePacket.InfoItem.Usda1Valid, singlePacket.InfoItem.Usda1, "")
+	gpsEvent.Usda2 = ReturnValueByCondition(singlePacket.InfoItem.Usda2Valid, singlePacket.InfoItem.Usda2, "")
+	gpsEvent.Usda3 = ReturnValueByCondition(singlePacket.InfoItem.Usda3Valid, singlePacket.InfoItem.Usda3, "")
+	gpsEvent.FaultCode = ReturnValueByCondition(singlePacket.InfoItem.FaultCodeValid, singlePacket.InfoItem.FaultCode, "")
+	gpsEvent.PosFlag = ReturnValueByCondition(singlePacket.PositioningModuleStatus, "1", "0")
+	gpsEvent.Ism = "0"
+	gpsEvent.GpsNum = singlePacket.NumberOfSatellitesItem.GpsSatelliteNumber
+	gpsEvent.BdNum = singlePacket.NumberOfSatellitesItem.BeidouSatelliteNumber
+	gpsEvent.Lpt = ReturnValueByCondition(singlePacket.InfoItem.LptValid, singlePacket.InfoItem.Lpt, "")
+	gpsEvent.Pt = ReturnValueByCondition(singlePacket.InfoItem.PtValid, singlePacket.InfoItem.Pt, "")
+	gpsEvent.Ct1 = ReturnValueByCondition(singlePacket.InfoItem.Ct1Valid, singlePacket.InfoItem.Ct1, "")
+	gpsEvent.Ct2 = ReturnValueByCondition(singlePacket.InfoItem.Ct2Valid, singlePacket.InfoItem.Ct2, "")
+	gpsEvent.Ambs = ReturnValueByCondition(singlePacket.InfoItem.AmbsValid, singlePacket.InfoItem.Ambs, "")
+	gpsEvent.Eis = ReturnValueByCondition(singlePacket.InfoItem.EisValid, singlePacket.InfoItem.Eis, "")
+	gpsEvent.Eos = ReturnValueByCondition(singlePacket.InfoItem.EosValid, singlePacket.InfoItem.Eos, "")
+	gpsEvent.Dchs = ReturnValueByCondition(singlePacket.InfoItem.DchsValid, singlePacket.InfoItem.Dchs, "")
+	gpsEvent.Sgs = ReturnValueByCondition(singlePacket.InfoItem.SgsValid, singlePacket.InfoItem.Sgs, "")
+	gpsEvent.Smv = ReturnValueByCondition(singlePacket.InfoItem.SmvValid, singlePacket.InfoItem.Smv, "")
+	gpsEvent.Ev = ReturnValueByCondition(singlePacket.InfoItem.EvValid, singlePacket.InfoItem.Ev, "")
+	gpsEvent.Dss = ReturnValueByCondition(singlePacket.InfoItem.DssValid, singlePacket.InfoItem.Dss, "")
+	gpsEvent.Drs = ReturnValueByCondition(singlePacket.InfoItem.DrsValid, singlePacket.InfoItem.Drs, "")
+	gpsEvent.Hs = ReturnValueByCondition(singlePacket.InfoItem.HsValid, singlePacket.InfoItem.Hs, "")
+	gpsEvent.Source = GPSEVENT_SOURCE_TCPSERVER
+	gpsEvent.Carrier = GPSEVENT_CARRIER_COSU
+
+	return gpsEvent
+}
+
+func ReturnValueByCondition(condition bool, trueVal string, falseVal string) string {
+	if condition {
+		return trueVal
+	} else {
+		return falseVal
+	}
 }
--- common/util/binaryutil_test.go
+++ common/util/binaryutil_test.go
@@ -7,6 +7,12 @@ import (
 )
 
 func TestBigEndianInt12(t *testing.T) {
+	// [6]:195
+	// [7]:87
+	// [8]:254
+	// [9]:143
+	// [10]:254
+	// [11]:131
 	b := []byte{0x20, 0x04}
 	result := BigEndianInt12(b)
 	assert.Equal(t, int16(-2047), result)
