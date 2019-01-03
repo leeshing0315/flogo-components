f9079572dcc38324e42317469639b5c6ac958309
Max C Li <max.c.li@cargosmart.com>
2019-01-03 10:11:50 +0800

    add send cmd integration test

--- sendPacket/main.go
+++ sendPacket/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"encoding/binary"
+	"net"
+)
 
 func main() {
 	// conn, err := net.Dial("tcp", "itciot-tcp.cargosmart.ai:8080")
@@ -11,7 +14,8 @@ func main() {
 	}
 	// sendLoginPacket(conn)
 	// sendSinglePacket(conn)
-	sendMultiPacket(conn)
+	// sendMultiPacket(conn)
+	testSendCmd(conn)
 }
 
 func sendLoginPacket(conn net.Conn) {
@@ -84,4 +88,90 @@ func sendMultiPacket(conn net.Conn) {
 	shouldBeNothing := make([]byte, 1)
 	conn.Read(shouldBeNothing)
 	println(shouldBeNothing)
+}
+
+func testSendCmd(conn net.Conn) {
+	// send login packet (first send devid)
+	n, err := conn.Write([]byte{80, 32, 84, 32, 73, 32, 73, 32, 80, 32, 80, 32, 13, 10,
+		50, 12, 207, 0, 27, 2, 15, 52, 54, 48, 48, 49, 49, 55, 49, 48, 51, 50, 52, 48, 56, 56,
+		6, 67, 48, 51, 54, 50, 53, // C03625
+		8, 72, 83, 49, 56, 49, 49, 48, 56, 17, 112})
+	println(n)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	loginAckBuffer := make([]byte, 7)
+	conn.Read(loginAckBuffer)
+
+	// send data packet
+	n, err = conn.Write([]byte{80, 32, 84, 32, 73, 32, 73, 32, 80, 32, 80, 32, 13, 10,
+		54, 12, 208, 0, 108, 0, 0, 0, 114, 24, 17, 33, 23, 67, 81, 1, 215, 31, 42, 7, 68, 30, 6, 0, 1, 0, 40, 100, 4, 2, 15, 10, 1, 33, 52, 54, 48, 48, 49, 49, 55, 49, 48, 51, 50, 52, 48, 56, 56, 67, 48, 48, 48, 48, 49, 83, 77, 85, 84, 48, 48, 48, 48, 48, 48, 49, 68, 2, 44, 68, 1, 255, 255, 255, 64, 195, 87, 254, 143, 254, 131, 255, 111, 255, 255, 255, 38, 6, 15, 210, 1, 182, 1, 142, 194, 183, 255, 195, 254, 99, 4, 63, 0, 208, 1, 72, 0, 0, 255, 193, 255, 195, 80, 246, 254})
+	println(n)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	dataAckBuffer := make([]byte, 7)
+	conn.Read(dataAckBuffer)
+
+	// receive cmd
+	setBuffer, err := parseByProtocol(conn)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	println(setBuffer)
+
+	// send cmd ack packet
+	n, err = conn.Write([]byte{80, 32, 84, 32, 73, 32, 73, 32, 80, 32, 80, 32, 13, 10,
+		54, 12, 209, 0, 0, 1, 1})
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	ackBuffer := make([]byte, 7)
+	conn.Read(ackBuffer)
+	println(ackBuffer)
+}
+
+func parseByProtocol(conn net.Conn) ([]byte, error) {
+	command := make([]byte, 1)
+	_, err := conn.Read(command)
+	if err != nil {
+		return nil, err
+	}
+
+	sequence := make([]byte, 2)
+	_, err = conn.Read(sequence)
+	if err != nil {
+		return nil, err
+	}
+
+	dataSegmentLength := make([]byte, 2)
+	_, err = conn.Read(dataSegmentLength)
+	if err != nil {
+		return nil, err
+	}
+
+	dataSegment := make([]byte, binary.BigEndian.Uint16(dataSegmentLength))
+	_, err = conn.Read(dataSegment)
+	if err != nil {
+		return nil, err
+	}
+
+	crc16Check := make([]byte, 2)
+	_, err = conn.Read(crc16Check)
+	if err != nil {
+		return nil, err
+	}
+
+	result := []byte{}
+	result = append(result, command...)
+	result = append(result, sequence...)
+	result = append(result, dataSegmentLength...)
+	result = append(result, dataSegment...)
+	result = append(result, crc16Check...)
+	return result, nil
 }
