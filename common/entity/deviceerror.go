7de305fb8d155c4089712d11c483fd4772883365
Max C Li <max.c.li@cargosmart.com>
2019-01-04 16:31:12 +0800

    add json name

--- common/entity/deviceerror.go
+++ common/entity/deviceerror.go
@@ -9,10 +9,10 @@ type DeviceError struct {
 	Seqno     string `json:"seqno"`
 	Devid     string `json:"devid"`
 	Faulttype string `json:"faulttype"`
-	Status    string `json:"devid"`
-	Logtime   string `json:"devid"`
-	Revtime   string `json:"devid"`
-	TableName string `json:"devid"` // default: "Tbldevicefault"
+	Status    string `json:"status"`
+	Logtime   string `json:"logtime"`
+	Revtime   string `json:"revtime"`
+	TableName string `json:"tableName"` // default: "Tbldevicefault"
 }
 
 func GenDeviceErrorsFromSinglePacket(singlePacket *SinglePacket, seqNo string, devId string) []string {
