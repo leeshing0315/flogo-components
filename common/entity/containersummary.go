6cbbe7ecdd8fd226d8c13ce0a7e2b5060a08f04a
Max C Li <max.c.li@cargosmart.com>
2019-01-11 16:51:23 +0800

    omit multi type field

--- common/entity/containersummary.go
+++ common/entity/containersummary.go
@@ -1,22 +1,22 @@
 package entity
 
 type ContainerSummary struct {
-	Simno      string `json:"simno"`
-	Carno      string `json:"carno"` // e.g. C04254
-	Carid      string `json:"carid"` // e.g. CXRU1495240
-	Commmode   string `json:"commmode"`
-	Unitcode   int    `json:"unitcode"`
-	Cartype    string `json:"cartype"`
-	Saveflag   int    `json:"saveflag"`
-	Calcflag   int    `json:"calcflag"`
-	Changeflag int    `json:"changeflag"`
+	Simno    string `json:"simno"`
+	Carno    string `json:"carno"` // e.g. C04254
+	Carid    string `json:"carid"` // e.g. CXRU1495240
+	Commmode string `json:"commmode"`
+	// Unitcode   int    `json:"unitcode"`
+	Cartype string `json:"cartype"`
+	// Saveflag   int    `json:"saveflag"`
+	// Calcflag   int    `json:"calcflag"`
+	// Changeflag int    `json:"changeflag"`
 	Changetime string `json:"changetime"`
 	Regtime    string `json:"regtime"`
 	Devtype    string `json:"devtype"`
-	Useacc     int    `json:"useacc"`
-	Groupname  string `json:"groupname"`
-	Checkflag  int    `json:"checkflag"`
-	Boxtype    string `json:"boxtype"`
-	Boxsize    string `json:"boxsize"`
-	TableName  string `json:"tableName"` // default: 'Tblcarbaseinfo'
+	// Useacc     int    `json:"useacc"`
+	Groupname string `json:"groupname"`
+	// Checkflag  int    `json:"checkflag"`
+	Boxtype   string `json:"boxtype"`
+	Boxsize   string `json:"boxsize"`
+	TableName string `json:"tableName"` // default: 'Tblcarbaseinfo'
 }
