7551a7b8f20157a4e66ddc5d29a207abab6fe678
Max C Li <max.c.li@cargosmart.com>
2019-01-11 16:37:48 +0800

    change cntrsummary type

--- common/entity/containersummary.go
+++ common/entity/containersummary.go
@@ -5,17 +5,17 @@ type ContainerSummary struct {
 	Carno      string `json:"carno"` // e.g. C04254
 	Carid      string `json:"carid"` // e.g. CXRU1495240
 	Commmode   string `json:"commmode"`
-	Unitcode   string `json:"unitcode"`
+	Unitcode   int    `json:"unitcode"`
 	Cartype    string `json:"cartype"`
-	Saveflag   string `json:"saveflag"`
-	Calcflag   string `json:"calcflag"`
-	Changeflag string `json:"changeflag"`
+	Saveflag   int    `json:"saveflag"`
+	Calcflag   int    `json:"calcflag"`
+	Changeflag int    `json:"changeflag"`
 	Changetime string `json:"changetime"`
 	Regtime    string `json:"regtime"`
 	Devtype    string `json:"devtype"`
-	Useacc     string `json:"useacc"`
+	Useacc     int    `json:"useacc"`
 	Groupname  string `json:"groupname"`
-	Checkflag  string `json:"checkflag"`
+	Checkflag  int    `json:"checkflag"`
 	Boxtype    string `json:"boxtype"`
 	Boxsize    string `json:"boxsize"`
 	TableName  string `json:"tableName"` // default: 'Tblcarbaseinfo'
