961b1250fe1897ae34c691eeb334dec1d328efb3
Max C Li <max.c.li@cargosmart.com>
2019-01-21 16:03:40 +0800

    fix cmd encode and decode

--- common/entity/deviceCofigCmd.go
+++ common/entity/deviceCofigCmd.go
@@ -1,7 +1,9 @@
 package entity
 
 import (
+	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"strconv"
 	"strings"
 
@@ -25,19 +27,22 @@ func GenSetConfigCommand(cmdVal map[string]string) (setConfigCommand []byte, err
 	powerOnFrequency := make([]byte, 5)
 	if val, found := cmdVal["01"]; found {
 		powerOnFrequency[0] = 1
-		copy(powerOnFrequency[1:], util.GetEndBytes(util.FromDecStrToHexStr(val), 4))
+		uintVal, _ := strconv.ParseUint(val, 10, 32)
+		binary.BigEndian.PutUint32(powerOnFrequency[1:], uint32(uintVal))
 	}
 
 	powerOffFrequency := make([]byte, 5)
 	if val, found := cmdVal["02"]; found {
 		powerOffFrequency[0] = 1
-		copy(powerOffFrequency[1:], util.GetEndBytes(util.FromDecStrToHexStr(val), 4))
+		uintVal, _ := strconv.ParseUint(val, 10, 32)
+		binary.BigEndian.PutUint32(powerOffFrequency[1:], uint32(uintVal))
 	}
 
 	collectFrequency := make([]byte, 5)
 	if val, found := cmdVal["03"]; found {
 		collectFrequency[0] = 1
-		copy(collectFrequency[1:], util.GetEndBytes(util.FromDecStrToHexStr(val), 4))
+		uintVal, _ := strconv.ParseUint(val, 10, 32)
+		binary.BigEndian.PutUint32(collectFrequency[1:], uint32(uintVal))
 	}
 
 	serverIPAndPort := make([]byte, 13)
@@ -47,6 +52,9 @@ func GenSetConfigCommand(cmdVal map[string]string) (setConfigCommand []byte, err
 		serverIPStr := array[0]
 		portStr := array[1]
 		ipSegmentStrs := strings.Split(serverIPStr, ".")
+		if len(ipSegmentStrs) != 4 {
+			return nil, errors.New("ipSegment length wrong")
+		}
 		i := 1
 		for _, ipSegmentStr := range ipSegmentStrs {
 			copy(serverIPAndPort[i:i+2], util.GetEndBytes(util.FromDecStrToHexStr(ipSegmentStr), 2))
@@ -58,18 +66,18 @@ func GenSetConfigCommand(cmdVal map[string]string) (setConfigCommand []byte, err
 	sleepMode := make([]byte, 2)
 	if val, found := cmdVal["05"]; found {
 		sleepMode[0] = 1
-		copy(sleepMode[1:], util.FromStrToUint32(val))
+		copy(sleepMode[1:], util.GetEndBytes(util.FromStrToUint32(val), 1))
 	}
 
-	content := make([]byte, 36)
-	copy(content[0:2], []byte{0x32, 0x41}) // * => 0x2A => (2=>0x32, A=>0x41)
-	copy(content[2:4], []byte{0x34, 0x43}) // L => 0x4C => (4=>0X34, C=>0x43)
-	copy(content[4:9], powerOnFrequency)
-	copy(content[9:14], powerOffFrequency)
-	copy(content[14:19], collectFrequency)
-	copy(content[19:32], serverIPAndPort)
-	copy(content[32:34], sleepMode)
-	copy(content[34:36], []byte{0x32, 0x33}) // # => 0x23 => (2=>0x32, 3=>0x33)
+	content := make([]byte, 33)
+	content[0] = 0x2A
+	content[1] = 0x4C
+	copy(content[2:7], powerOnFrequency)
+	copy(content[7:12], powerOffFrequency)
+	copy(content[12:17], collectFrequency)
+	copy(content[17:30], serverIPAndPort)
+	copy(content[30:32], sleepMode)
+	content[32] = 0x23
 
 	return content, nil
 }
@@ -80,8 +88,8 @@ func DecodeReadConfigAck(original []byte) string {
 	str.WriteString("*L")
 
 	// powerOnFrequency
-	str.WriteString(strconv.FormatUint(uint64(original[4]), 10))
-	for i := 5; i < 9; i++ {
+	str.WriteString(strconv.FormatUint(uint64(original[2]), 10))
+	for i := 3; i < 7; i++ {
 		if strconv.FormatUint(uint64(original[i]), 10) == "0" {
 			str.WriteString("0")
 		} else {
@@ -91,8 +99,8 @@ func DecodeReadConfigAck(original []byte) string {
 	}
 
 	// powerOffFrequency
-	str.WriteString(strconv.FormatUint(uint64(original[9]), 10))
-	for i := 10; i < 14; i++ {
+	str.WriteString(strconv.FormatUint(uint64(original[7]), 10))
+	for i := 8; i < 12; i++ {
 		if strconv.FormatUint(uint64(original[i]), 10) == "0" {
 			str.WriteString("0")
 		} else {
@@ -102,8 +110,8 @@ func DecodeReadConfigAck(original []byte) string {
 	}
 
 	// collectFrequency
-	str.WriteString(strconv.FormatUint(uint64(original[14]), 10))
-	for i := 15; i < 19; i++ {
+	str.WriteString(strconv.FormatUint(uint64(original[12]), 10))
+	for i := 13; i < 17; i++ {
 		if strconv.FormatUint(uint64(original[i]), 10) == "0" {
 			str.WriteString("0")
 		} else {
@@ -113,8 +121,8 @@ func DecodeReadConfigAck(original []byte) string {
 	}
 
 	// serverIPAndPort
-	str.WriteString(strconv.FormatUint(uint64(original[19]), 10))
-	for i := 20; i < 32; i++ {
+	str.WriteString(strconv.FormatUint(uint64(original[17]), 10))
+	for i := 18; i < 30; i++ {
 		if strconv.FormatUint(uint64(original[i]), 10) == "0" {
 			str.WriteString("0")
 		} else {
@@ -124,8 +132,8 @@ func DecodeReadConfigAck(original []byte) string {
 	}
 
 	//sleepMode
-	str.WriteString(strconv.FormatUint(uint64(original[32]), 10))
-	str.WriteString(strconv.FormatUint(uint64(original[33]), 10))
+	str.WriteString(strconv.FormatUint(uint64(original[30]), 10))
+	str.WriteString(strconv.FormatUint(uint64(original[31]), 10))
 
 	str.WriteString("#")
 
