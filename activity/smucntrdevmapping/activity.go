c208b9a0890cb05fb46ceaaeb69fa9c6205f5ce8
Max C Li <max.c.li@cargosmart.com>
2019-01-25 16:58:19 +0800

    add Activity smucntrdevmapping

---
+++ activity/smucntrdevmapping/activity.go
@@ -0,0 +1,41 @@
+package smucntrdevmapping
+
+import (
+	"encoding/json"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/leeshing0315/flogo-components/common/entity"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+	cntrDevMappingStr := context.GetInput("cntrDevMapping").(string)
+
+	// do eval
+	var cntrDevMapping *entity.ContainerDeviceMapping
+	err = json.Unmarshal([]byte(cntrDevMappingStr), cntrDevMapping)
+	if err != nil {
+		return false, err
+	}
+
+	context.SetOutput("sim", cntrDevMapping.Simno)
+	context.SetOutput("devId", cntrDevMapping.DeviceId)
+	context.SetOutput("cntrNum", cntrDevMapping.ContainerNumber)
+
+	return true, nil
+}
