f2bf1f105fed197e0df3d22a8fd07ba0309e948e
Max C Li <max.c.li@cargosmart.com>
2019-01-29 10:27:03 +0800

    use map to unmarshal instead of entity

--- activity/smucntrdevmapping/activity.go
+++ activity/smucntrdevmapping/activity.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
-	"github.com/leeshing0315/flogo-components/common/entity"
 )
 
 // MyActivity is a stub for your Activity implementation
@@ -27,15 +26,15 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	cntrDevMappingStr := context.GetInput("cntrDevMapping").(string)
 
 	// do eval
-	var cntrDevMapping = entity.ContainerDeviceMapping{}
+	var cntrDevMapping = make(map[string]interface{})
 	err = json.Unmarshal([]byte(cntrDevMappingStr), &cntrDevMapping)
 	if err != nil {
 		return false, err
 	}
 
-	context.SetOutput("sim", cntrDevMapping.Simno)
-	context.SetOutput("devId", cntrDevMapping.DeviceId)
-	context.SetOutput("cntrNum", cntrDevMapping.ContainerNumber)
+	context.SetOutput("sim", cntrDevMapping["simno"])
+	context.SetOutput("devId", cntrDevMapping["carno"])
+	context.SetOutput("cntrNum", cntrDevMapping["carid"])
 
 	return true, nil
 }
