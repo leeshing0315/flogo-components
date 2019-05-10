f481fe99016abf2fa59c609f8b31845f6f32dbd7
Max C Li <max.c.li@cargosmart.com>
2019-05-10 11:34:17 +0800

    originalpacket add column bytesLength

--- common/entity/originalpacket.go
+++ common/entity/originalpacket.go
@@ -3,12 +3,13 @@ package entity
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type OriginalPacket struct {
-	Id       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Protocol string             `json:"protocol,omitempty"`
-	Ip       string             `json:"ip,omitempty"`
-	RevTime  string             `json:"revTime,omitempty"`
-	Bytes    []byte             `json:"bytes,omitempty"`
-	Pin      string             `json:"pin,omitempty"`
+	Id          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Protocol    string             `json:"protocol,omitempty"`
+	Ip          string             `json:"ip,omitempty"`
+	RevTime     string             `json:"revTime,omitempty"`
+	Bytes       []byte             `json:"bytes,omitempty"`
+	BytesLength int                `json:"bytesLength,omitempty" bson:"bytesLength,omitempty"`
+	Pin         string             `json:"pin,omitempty"`
 
 	Source string `json:"source,omitempty"`
 }
