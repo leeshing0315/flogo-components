001cc9137efaaf9c132af748c84c43e57d27df4f
Max C Li <max.c.li@cargosmart.com>
2019-05-16 16:43:46 +0800

    add recoverutil

---
+++ common/util/recoverutil.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"context"
+	"os"
+
+	"github.com/leeshing0315/flogo-components/common/entity"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var mongoClient *mongo.Client
+var coll *mongo.Collection
+
+func init() {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		panic(err)
+	}
+	mongoClient = client
+	mongoDb := client.Database("iot")
+	coll = mongoDb.Collection("tcpServerExceptions")
+}
+
+func LogDownException(eventTime string, pin string, originalPacket []byte, err error) {
+	exception := &entity.TcpServerException{
+		RevTime:      eventTime,
+		Pin:          pin,
+		ProtocolType: entity.PROTOCOL_TYPE_SMU,
+		Bytes:        originalPacket,
+		ErrorType:    entity.ERROR_TYPE_UNCHECKED,
+		ErrorReason:  err.Error(),
+	}
+	coll.InsertOne(context.Background(), exception)
+}
